261b91d101eb8b32ef25900de633806fb8fc92d7
way-key <45gqz7@gmail.com>
2020-03-24 18:56:43 +0900

    [Update]5章削除機能まで

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -5,12 +5,24 @@ class BooksController < ApplicationController
     	@books = Book.all
     end
     def create
-    	@book = Book.new(book_params)
-    	@book.user_id = current_user.id
-    	@book.save
-    	redirect_to books_path# 後でshowに修正
+    	book = Book.new(book_params)
+    	book.user_id = current_user.id
+    	if book.save
+    	redirect_to book_path(book.id)
+    	else
+    	@book = Book.new
+    	@books = Book.all
+    	flash[:notice] = "error"
+    	render :index
+    	end
     end
     def show
+    	@book = Book.find(params[:id])
+    end
+    def destroy
+    	@book = Book.find(params[:id])
+    	@book.destroy
+    	redirect_to books_path
     end
     private
     def book_params
