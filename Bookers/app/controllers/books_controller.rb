16bd0950b7994c45bb603d3f103d5765d778f3ef
way-key <45gqz7@gmail.com>
2020-03-24 17:24:02 +0900

    [Update]第3章の内容続き(修正)

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -5,9 +5,15 @@ class BooksController < ApplicationController
     	@books = Book.all
     end
     def create
-    	
+    	@book = Book.new(book_params)
+    	@book.user_id = current_user.id
+    	@book.save
+    	redirect_to books_path# 後でshowに修正
     end
     def show
     end
-
+    private
+    def book_params
+    	params.require(:book).permit(:title, :body)
+    end
 end
